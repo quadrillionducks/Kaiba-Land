08f214ab07c9c124d1d679237d5720ab0b2b5c0a
quadrillionducks <ducksfan046@gmail.com>
2024-04-18 13:55:39 -0500

    initial commit

---
+++ ygocards.py
@@ -0,0 +1,398 @@
+kaibadeck = {
+    "blue-eyes": {
+        "description": "This legendary dragon is a powerful engine of destruction. Virtually invincible, very few have faced this awesome creature and lived to tell the tale.",
+        "ATK": 3000,
+        "DEF": 2500,
+        "level": 8,
+        "form": 1
+    },
+    "Battle Ox": {
+        "description": "A monster with tremendous power, it destroys enemies with a swing of its axe.",
+        "ATK": 1700,
+        "DEF": 1000,
+        "level": 4,
+        "form": 1
+    },
+    "Hitotsu-me Giant": {
+        "description": "A one-eyed behemoth with thick, long fur that conceals sharp claws for ripping enemies to shreds.",
+        "ATK": 1200,
+        "DEF": 1000,
+        "level": 4,
+        "form": 1
+    },
+    "Ryu-Kishin Powered": {
+        "description": "A wicked dragon that lays waste to all who dare challenge it.",
+        "ATK": 1600,
+        "DEF": 1200,
+        "level": 4,
+        "form": 1
+    },
+    "Saggi the Dark Clown": {
+        "description": "A mischievous clown who enjoys playing cruel tricks on unsuspecting foes.",
+        "ATK": 600,
+        "DEF":1500,
+        "level":3,
+        "form": 1
+    },
+    "Luster Dragon": {
+        "description": "This dragon dazzles enemies with its shimmering scales before striking them down with its powerful tail.",
+        "ATK": 1900,
+        "DEF": 1600,
+        "level": 4,
+        "form": 1
+    },
+    "Vorse Raider": {
+        "description": "A swift warrior on horseback, it charges at enemies with lightning speed, leaving destruction in its wake.",
+        "ATK": 1900,
+        "DEF": 1200,
+        "level": 4,
+        "form": 1
+    },
+    "Lord of D.": {
+        "description": "A powerful dragon lord who commands other dragons to do his bidding.",
+        "ATK": 1200,
+        "DEF": 1100,
+        "level": 4,
+        "form": 1,
+        "E":1
+    },
+    "Blue-Eyes Alternative Dragon": {
+        "description": "An alternate form of the legendary Blue-Eyes White Dragon, it unleashes devastating attacks on its foes.",
+        "ATK": 3000,
+        "DEF": 2500,
+        "level": 8,
+        "form": 1,
+        "E": 2
+    },
+    "Alexandrite Dragon": {
+        "description": "A dragon adorned with brilliant gems, it dazzles enemies with its radiant light before delivering a deadly blow.",
+        "ATK": 2000,
+        "DEF": 100,
+        "level": 4,
+        "form": 1
+    },
+    "Pot of Greed": {
+        "description": "Draw 2 cards from your Deck.",
+        "form": 3,
+        "E": 3
+    },
+    "Burst Stream of Destruction": {
+        "description": "You can only activate this card when you control a face-up 'Blue-Eyes White Dragon'. Destroy all monsters your opponent controls.",
+        "form": 3,
+        "E": 4
+    },
+    "Magic Cylinder": {
+        "description": "Negate the attack of 1 of your opponent's monsters and inflict damage equal to its ATK to your opponent's Life Points.",
+        "form": 3,
+        "E":4
+    },
+    "Polymerization": {
+        "description": "Fusion Summon 1 Fusion Monster from your Extra Deck, using monsters from your hand or your side of the field as Fusion Materials.",
+        "form": 3,
+        "E":5
+    },
+    "Monster Reborn": {
+        "description": "Target 1 monster in either player's Graveyard; Special Summon it.",
+        "form": 3,
+        "E":6
+    },
+    "Dark Hole": {
+        "description": "Destroy all monsters on the field.",
+        "form": 3,
+        "E":7
+    },
+    "De-Spell": {
+        "description": "Target 1 Spell Card on the field; destroy that target.",
+        "form": 3,
+        "E":8
+    },
+    "Flute of Summoning Dragon": {
+        "description": "Special Summon up to 2 Dragon-Type monsters from your hand.",
+        "form": 3,
+        "E":9
+    },
+    "Chaos Form": {
+        "description": "This card can only be used to Ritual Summon any 1 'Black Luster Soldier' Ritual Monster. You must also Tribute monsters from your hand or field whose total Levels equal the Level of the Ritual Monster you Ritual Summon. If a 'Black Luster Soldier' monster you control would be destroyed by battle or card effect, you can banish this card from your Graveyard instead.",
+        "form": 3,
+        "E":10
+    },
+    "Crush Card Virus": {
+        "description": "Tribute 1 DARK monster with 1000 or less ATK; your opponent takes no damage until the end of the next turn after this card resolves, also, you look at your opponent's hand and all monsters they control, and if you do, destroy the monsters among them with 1500 or more ATK, then your opponent can destroy up to 3 monsters with 1500 or more ATK in their Deck.",
+        "form": 4,
+        "E":11
+    },
+    "Ring of Destruction": {
+        "description": "Destroy 1 face-up monster and inflict damage to both players equal to its ATK.",
+        "form": 4,
+        "E":12
+    },
+    "Mirror Force": {
+        "description": "When an opponent's monster declares an attack: Destroy all Attack Position monsters your opponent controls.",
+        "form": 4,
+        "E":13
+    }
+}
+yugideck = {
+    "Exodia The Forbidden One": {
+        "description": "When you have 'Right Leg of the Forbidden One', 'Left Leg of the Forbidden One', 'Right Arm of the Forbidden One', and 'Left Arm of the Forbidden One' in addition to this card in your hand, you win the Duel.",
+        "ATK": 1000,
+        "DEF": 1000,
+        "level": 3,
+        "form": 1,
+        "E":14
+    },
+    "Right Arm of the Forbidden One": {
+        "description": "One of the pieces of 'Exodia The Forbidden One'.",
+        "ATK": 200,
+        "DEF": 300,
+        "level": 1,
+        "form": 1
+    },
+    "Left Arm of the Forbidden One": {
+        "description": "One of the pieces of 'Exodia The Forbidden One'.",
+        "ATK": 200,
+        "DEF": 300,
+        "level": 1,
+        "form": 1
+    },
+    "Right Leg of the Forbidden One": {
+        "description": "One of the pieces of 'Exodia The Forbidden One'.",
+        "ATK": 200,
+        "DEF": 300,
+        "level": 1,
+        "form": 1
+    },
+    "Left Leg of the Forbidden One": {
+        "description": "One of the pieces of 'Exodia The Forbidden One'.",
+        "ATK": 200,
+        "DEF": 300,
+        "level": 1,
+        "form": 1
+    },
+    "Gaia The Fierce Knight": {
+        "description": "A knight whose horse travels faster than the wind.",
+        "ATK": 2300,
+        "DEF": 2100,
+        "level": 7,
+        "form":1
+    },
+    "Curse of Dragon": {
+        "description": "A wicked dragon that taps into dark forces to execute a powerful attack.",
+        "ATK": 2000,
+        "DEF": 1500,
+        "level": 5,
+        "form": 1
+    },
+    "Dark Magician": {
+        "description": "The ultimate wizard in terms of attack and defense.",
+        "ATK": 2500,
+        "DEF": 2100,
+        "level": 7,
+        "form": 1
+    },
+    "Dark Magician Girl": {
+        "description": "The female counterpart of the Dark Magician.",
+        "ATK": 2000,
+        "DEF": 1700,
+        "level": 6,
+        "form": 1,
+        "E":15
+    },
+    "Celtic Guardian": {
+        "description": "An elf who learned to wield a sword, he baffles enemies with lightning-swift attacks.",
+        "ATK": 1400,
+        "DEF": 1200,
+        "level": 4,
+        "form": 1
+    },
+    "Black Luster Soldier": {
+        "description": "A warrior fueled by dark magic.",
+        "ATK": 3000,
+        "DEF": 2500,
+        "level": 8,
+        "form": 5
+    },
+    "Summoned Skull": {
+        "description": "A fiend with dark powers for confusing the enemy. Among the Fiend-Type monsters, this monster boasts considerable force.",
+        "ATK": 2500,
+        "DEF": 1200,
+        "level": 6,
+        "form": 1
+    },
+    "Kuriboh": {
+        "description": "A monster with the power of multiplication that can create an army from a single card.",
+        "ATK": 300,
+        "DEF": 200,
+        "level": 1,
+        "form": 1,
+        "E":16
+    },
+    "Black Magician of Chaos": {
+        "description": "The ultimate wizard in terms of attack and defense.",
+        "ATK": 2800,
+        "DEF": 2600,
+        "level": 7,
+        "form": 5
+    },
+    "Double Coston": {
+        "description": "A fiend that can use its own hands and feet as weapons.",
+        "ATK": 1700,
+        "DEF": 1650,
+        "level": 4,
+        "form": 1,
+        "E":17
+    },
+    "Giant Soldier of Stone": {
+        "description": "A giant warrior made of stone. A punch from this creature has earth-shaking results.",
+        "ATK": 1300,
+        "DEF": 2000,
+        "level": 3,
+        "form": 1
+    },
+    "Slifer The Sky Dragon": {
+        "description": "This card cannot be Special Summoned. To Tribute Summon this card, you must Tribute 3 monsters. The Normal Summon of this card cannot be negated. When this card is Normal Summoned, it is unaffected by Spell/Trap effects and by activated effects from any monster whose original Level/Rank is lower than this card's current Level. Once per turn, during the End Phase, if this card was Special Summoned: Send it to the GY. Gains 1000 ATK and DEF for each card in your hand. If a monster(s) is Normal or Special Summoned to your opponent's field in Attack Position: That monster(s) loses 2000 ATK, then if its ATK has been reduced to 0 as a result, destroy it.",
+        "ATK": 0,
+        "DEF": 0,
+        "level": 10,
+        "form": 1,
+        "E":18
+    },
+    "Cyber Jar": {
+        "description": "FLIP: Destroy all monsters on the field (both face-up and face-down), then both players reveal the top 5 cards from their Decks, then Special Summon all revealed Level 4 or lower monsters in face-up Attack Position or face-down Defense Position, also add any remaining cards to the hand. (If either player has less than 5 cards in their Deck, reveal as many as possible.)",
+        "ATK": 900,
+        "DEF": 900,
+        "level": 3,
+        "form":1,
+        "E":19
+    },
+    "Buster Blader": {
+        "description": "This card gains 500 ATK for each Dragon-Type monster your opponent controls or is in their Graveyard.",
+        "ATK": 2600,
+        "DEF": 2300,
+        "level": 7,
+        "form": 1,
+        "E":20
+    },
+    "Mirror Force": {
+        "description": "When an opponent's monster declares an attack: Destroy all Attack Position monsters your opponent controls.",
+        "form": 4,
+        "E":13
+    },
+    "Change of Heart": {
+        "description": "Target 1 monster your opponent controls; take control of it until the End Phase.",
+        "form": 3,
+        "E":21
+    },
+    "Fiber Jar": {
+        "description": "FLIP: Both players send all cards they currently control to the Graveyard, then both players shuffle their hands and Decks, then each player draws 5 cards.",
+        "ATK": 500,
+        "DEF": 500,
+        "level": 3,
+        "form": 1,
+        "E":22
+    },
+    "Polymerization": {
+        "description": "Fusion Summon 1 Fusion Monster from your Extra Deck, using monsters from your hand or your side of the field as Fusion Materials.",
+        "form": 3,
+        "E":5
+    },
+    "Chaos Form": {
+        "description": "This card can only be used to Ritual Summon any 1 'Black Luster Soldier' Ritual Monster. You must also Tribute monsters from your hand or field whose total Levels equal the Level of the Ritual Monster you Ritual Summon. If a 'Black Luster Soldier' monster you control would be destroyed by battle or card effect, you can banish this card from your Graveyard instead.",
+        "form": 3,
+        "E":10
+    },
+    "Swords of Revealing Light": {
+        "description": "After this card's activation, it remains on the field, but destroy it during the End Phase of your opponent's 3rd turn. When this card is activated: If your opponent controls any face-down monsters, flip them face-up. While this card is face-up on the field, your opponent's monsters cannot declare an attack.",
+        "form": 3,
+        "E":25
+    },
+    "Pot of Greed": {
+        "description": "Draw 2 cards from your Deck.",
+        "form": 3,
+        "E":3
+    },
+    "Monster Reborn": {
+        "description": "Target 1 monster in either player's Graveyard; Special Summon it.",
+        "form": 3,
+        "E":6
+    },
+    "Silent Swordsman Lv3": {
+        "description": "Negate all Spell effects on the field.",
+        "ATK": 1000,
+        "DEF": 1000,
+        "level": 3,
+        "form": 1,
+        "E":23
+    },
+    "Silent Swordsman Lv5": {
+        "description": "This card cannot be Normal Summoned or Set. This card can only be Special Summoned by Tribute Summoning 'Silent Swordsman LV3' on your side of the field. While this card is face-up on the field, your opponent cannot activate Spell Cards. If 'Silent Swordsman LV5' is destroyed, you can Special Summon 1 'Silent Swordsman LV7' from your hand or Deck.",
+        "ATK": 2300,
+        "DEF": 1000,
+        "level": 5,
+        "form": 1,
+        "E":24
+    }
+}
+kextra = {
+    "Azure-Eyes Silver Dragon": {
+        "description": "When this card is Special Summoned: Dragon-Type monsters you currently control cannot be targeted or destroyed by card effects until the end of the next turn. Once per turn, during your Standby Phase: You can target 1 Normal Monster in your Graveyard; Special Summon that target.",
+        "ATK": 2500,
+        "DEF": 3000,
+        "level": 9,
+        "form": 6,
+        "E":26
+    },
+    "Blue-Eyes Ultimate Dragon": {
+        "description": "Blue-Eyes White Dragon + Blue-Eyes White Dragon + Blue-Eyes White Dragon",
+        "ATK": 4500,
+        "DEF": 3800,
+        "level": 12,
+        "form": 5
+    }
+}
+yextra = {
+    "Gaia The Dragon Champion": {
+        "description": "This card can only be Special Summoned by offering 'Gaia The Fierce Knight' on the field as a Tribute. When this card is Special Summoned, you can target 1 Level 5 or lower Dragon-Type monster in your Graveyard; Special Summon that target. This card gains 2600 ATK if a 'Black Luster Soldier' monster is on the field.",
+        "ATK": 2600,
+        "DEF": 2100,
+        "level": 7,
+        "form": 5,
+        "E":27
+    },
+    "Dark Paladin": {
+        "description": "This card gains 500 ATK for each Dragon-Type monster on the field and in either player's Graveyard. You can discard 1 card to negate the activation of a Spell Card and destroy it.",
+        "ATK": 2900,
+        "DEF": 2400,
+        "level": 8,
+        "form": 5,
+        "E":28
+    },
+    "The Dark Magicians": {
+        "description": "Dark Magician + Dark Magician",
+        "ATK": 2800,
+        "DEF": 2300,
+        "level": 9,
+        "form": 5,
+        "E":29
+    },
+    "Dark Magician the Dragon Knight": {
+        "description": "Dark Magician + 1 Dragon-Type monster When a Spell/Trap Card, or monster effect, is activated that targets this face-up card on the field (Quick Effect): You can discard the same type of card (Monster, Spell, or Trap); negate the effect.",
+        "ATK": 3000,
+        "DEF": 2500,
+        "level": 8,
+        "form": 5,
+        "E":30
+    }
+}
+
+print(yextra)
+
+
+print(kextra)
+
+
+print(yugideck)
+
+
+print(kaibadeck)
+
